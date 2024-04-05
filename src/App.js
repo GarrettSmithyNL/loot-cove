12f3291d664dfd169b5e81fec043b29d3288ec54
garrettsmithynl <garrett_smith_30@hotmail.com>
2024-04-05 18:01:11 -0230

    added the photos

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-import { getGameById, getGames } from "./data/api";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
-  const games = getGames();
-  console.log(games);
+  let [cartById, setCartById] = useState([4, 6, 4, 25, 1, 10, 21]);
+
+  useEffect(() => {
+    sortGames();
+  }, [cartById]);
+
+  let sortGames = () => {
+    setCartById(cartById.sort());
+  };
+
+  let removeGame = (id) => {
+    cartById.splice(cartById.indexOf(id), 1);
+    setCartById(cartById);
+  };
 
   return (
     <Router>
       <div className="App">
+        <h1>Loot Cove</h1>
         <Routes>
           <Route
             path="/"
             element={
               <div>
-                <h1>Title: {games[1].name}</h1>
+                <h1>Title: Garrett</h1>
               </div>
             }
           ></Route>
           <Route
-            path="/2"
+            path="/cart"
             element={
-              <div>
-                <h1>Title: {games[2].name}</h1>
-              </div>
+              <ShoppingCart
+                cartById={cartById}
+                onClick={removeGame}
+              />
             }
           ></Route>
         </Routes>
